ec475bcc95ecd1037d515215a57f69fdd30c7cae
Dj Ekler <robertplaudis674@gmail.com>
2022-12-28 18:41:17 +0300

    fix cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,6 @@
 import readlineSync from 'readline-sync';
 
-const name = (userName) => {
-  userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-};
+const userName = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${userName}!`);
 
-export { name };
+export default userName;
