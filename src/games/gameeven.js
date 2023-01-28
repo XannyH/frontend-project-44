d9f3c21f78dc81697e6847216c2d455456ceb4f1
Dj Ekler <robertplaudis674@gmail.com>
2023-01-28 12:44:13 +0300

    fix gameeven.js

--- src/games/gameeven.js
+++ src/games/gameeven.js
@@ -1,28 +1,20 @@
-import readlineSync from 'readline-sync';
-import greetings from './cli.js';
+import { startGame, numberGeneration } from '../index.js';
 
+const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const numberGeneration = () => Math.floor(Math.random() * 100);
-
-export default () => {
-  const userName = greetings();
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  for (let i = 0; i < 3; i += 1) {
-    let num = numberGeneration();
-    console.log(`Question: ${num}`);
-    let rightAnswer = 'no';
-    if (num % 2 === 0) {
-      rightAnswer = 'yes';
-    }
-    let userAnswer = readlineSync.question('Your answer: ');
-    if (userAnswer !== rightAnswer) {
-      console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${rightAnswer}"\nLet's try again, ${userName}!`);
-      break;
-    } else {
-      console.log('Correct!');
-    }
-    if (i === 2) {
-      console.log(`Congratulations, ${userName}!`);
-    }
+export const isEven = (number) => {
+  if (number % 2 == 0) {
+    return 'yes';
+  } else {
+    return 'no';
   }
-}
+};
+
+const generateRound = () => {
+  const number = numberGeneration();
+  const rightAnswer = isEven(number);
+  const question = `${number}`;
+  return [question, rightAnswer];
+};
+
+export default () => startGame(description, generateRound);
