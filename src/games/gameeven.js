889ae7b7facc4bb78743833ee951e6e13003af38
Xan <robertplaudis674@gmail.com>
2023-02-18 12:24:01 +0300

    fix logic + linter errors

--- src/games/gameeven.js
+++ src/games/gameeven.js
@@ -1,19 +1,24 @@
-import { startGame, numberGeneration } from '../index.js';
+import { startGame } from '../index.js';
+import { randomIntFromInterval } from '../utils.js';
 
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 export const isEven = (number) => {
-  if (number % 2 == 0) {
-    return 'yes';
-  } else {
-    return 'no';
+  if (number % 2 === 0) {
+    return true;
   }
+  return false;
 };
 
 const generateRound = () => {
-  const number = numberGeneration();
-  const rightAnswer = isEven(number);
-  const question = `${number}`;
+  const number = randomIntFromInterval(1, 100);
+  let rightAnswer = '';
+  if (isEven(number) === true) {
+    rightAnswer = 'yes';
+  } else {
+    rightAnswer = 'no';
+  }
+  const question = String(number);
   return [question, rightAnswer];
 };
 
